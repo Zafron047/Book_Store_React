2f8069dbd963b46984358a8725cd7506b1099bab
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-07-19 16:26:56 +0600

    Add Redux with toolkit and Create booksSlice.js

---
+++ src/redux/booksSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push({
+        id: action.payload.id,
+        title: action.payload.title,
+        author: action.payload.author,
+      },);
+    },
+    removeBook: (state, action) => {
+      const bookIdToRemove = action.payload.id;
+      state.books = state.books.filter((book) => book.id !== bookIdToRemove);
+    },
+  },
+});
+
+export const booksReducer = booksSlice.reducer;
+export const { addBook, removeBook } = booksSlice.actions;
